d6330c74244aadb6aa68fbddeabd0b7d50d13b38
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-06-28 15:34:45 +0200

    Create d'une Agency

--- agency-service.js
+++ agency-service.js
@@ -3,8 +3,11 @@ const responder = new cote.Responder({ name: 'agency responder' })
 
 // DB mocking
 const agencies = [
-  { id: 1, name: 'Agency 1' }
+  { id: 1, name: 'Agency 1', userId: 1 }
 ]
 
 responder.on('index', ({ type }, cb) => cb(agencies))
-responder.on('create', ({ type, agency }, cb) => cb(agencies.push(agency)))
+responder.on('create', ({ type, agency }, cb) => {
+  agencies.push(agency)
+  cb(agency)
+})
