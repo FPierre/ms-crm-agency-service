054b238c53719dd6299387c3f5248134dfb17c37
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-06-28 14:35:16 +0200

    Index des agencies

--- agency-service.js
+++ agency-service.js
@@ -1,7 +1,10 @@
 const cote = require('cote')
-const responder = new cote.Responder({ name: 'agency create responder', key: 'create' })
+const responder = new cote.Responder({ name: 'agency responder' })
 
 // DB mocking
-const agencies = []
+const agencies = [
+  { id: 1, name: 'Agency 1' }
+]
 
+responder.on('index', ({ type }, cb) => cb(agencies))
 responder.on('create', ({ type, agency }, cb) => cb(agencies.push(agency)))
