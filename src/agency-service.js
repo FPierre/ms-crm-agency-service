c4e38ca3b6514aa694afe44e74a663ceb21bc25c
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-07 15:13:27 +0200

    WIP tests avec docker-compose

--- src/agency-service.js
+++ src/agency-service.js
@@ -3,20 +3,7 @@ const cote = require('cote')
 const Agency = require('./agency')
 
 const responder = new cote.Responder({ name: 'agency responder', key: 'agency' })
-const requester = new cote.Requester({ name: 'log requester', key: 'log' })
-
-// DB mocking
-const activities = {
-  0: 'frenchFriesStand',
-  1: 'candyFlossStand'
-}
-
-const commercialStatus: {
-  0: 'client',
-  1: 'prospect',
-  2: 'inactive',
-  3: 'oldCustomer'
-}
+const logRequester = new cote.Requester({ name: 'log requester', key: 'log' })
 
 responder.on('index', ({ type }, cb) => {
   return Agency.findAll()
@@ -27,31 +14,20 @@ responder.on('show', ({ type, id }, cb) => {
 })
 
 responder.on('create', ({ type, agency }, cb) => {
-  // Log entry
-  requester.send({ type: 'create', event: 'create' }, res => {
-    agencies.push(agency)
+  logEntry({}, agency, 'create')
+})
+
+responder.on('update', ({ type, agency }, cb) => {
+  Agency.findById(agency.id).then(agency => {
+    logEntry({}, agency, 'update')
     cb(agency)
   })
 })
 
-responder.on('update', ({ type, agency }, cb) => {
-  // Log entry
-  requester.send({ type: 'create', event: 'update' }, res => {
-    const agencyIndex = agencies.findIndex(a => a.id === agency.id)
-
-    if (agencyIndex) {
-      agencies[agencyIndex] = agency
-      cb(agency)
-    } else {
-      cb({})
-    }
-  })
-})
-
 responder.on('delete', ({ type, agencyIndex }, cb) => {
-  // Log entry
-  requester.send({ type: 'create', event: 'delete' }, res => {
-    agencies.splice(agencyIndex, 1)
-    cb({})
-  })
+
 })
+
+function logEntry (user, object, event) {
+  return logRequester.send({ type: 'create', user, object, event })
+}
