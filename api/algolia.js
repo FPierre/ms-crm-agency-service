0a7ab5e6a7b40d60b3185f652a4cecdd15731bbb
FPierre <pflauder@gmail.com>
2017-07-15 09:10:28 +0200

    Client et serializer Algolia

--- api/algolia.js
+++ api/algolia.js
@@ -1,6 +1,34 @@
 const algoliasearch = require('algoliasearch')
+const moment = require('moment')
 const env = require('../env/dev.env.js')
+
 const client = algoliasearch(env.algoliaAppId, env.algoliaApiKey)
 const index = client.initIndex(env.algoliaIndex)
 
-module.exports = index
+function serialize(agency) {
+  return {
+    objectID: agency._id,
+    name: agency.name,
+    phone: agency.phone,
+    street: agency.street,
+    country: agency.country,
+    city: agency.city,
+    zipCode: agency.zipCode,
+    website: agency.website,
+    activities: agency.activities,
+    commercialStatus: agency.commercialStatus,
+    createdAt: moment(agency.createdAt).unix(),
+    updatedAt: moment(agency.updatedAt).unix(),
+    _authorId: agency._authorId,
+    _responsibleId: agency._responsibleId,
+    _geoloc: {
+      lat: agency.lat,
+      lng: agency.lng
+    }
+  }
+}
+
+module.exports = {
+  algoliaClient: index,
+  algoliaSerialize: serialize
+}
